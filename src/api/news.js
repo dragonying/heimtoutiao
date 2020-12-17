d7636dc4f6148187a820950a273f63ae77aecc01
月光色 <632352703@qq.com>
2020-12-17 17:00:25 +0800

    newapi

--- src/api/news.js
+++ src/api/news.js
@@ -33,3 +33,21 @@ export const replyComments = (target, isDelete = false) => {
   isDelete ? (option.data = target) : (option.params = target)
   return request(option)
 }
+// 收藏文章和取消收藏文章
+export const collectArticle = (target, isDelete = false) => {
+  const option = {
+    url: '/app/v1_0/article/collections',
+    method: isDelete ? 'DELETE' : 'POST'
+  }
+  isDelete ? (option.url += `/${target}`) : (option.data = { target })
+  return request(option)
+}
+// 对评论或评论回复点赞
+export const commentLike = (target, isDelete = false) => {
+  const option = {
+    url: '/app/v1_0/comment/likings',
+    method: isDelete ? 'DELETE' : 'POST'
+  }
+  isDelete ? (option.url += `/${target}`) : (option.data = { target })
+  return request(option)
+}
